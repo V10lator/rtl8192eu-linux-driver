17aaac218d3d4b01f1eb085db4802344816667ba
V10lator <v10lator@myway.de>
2017-09-21 08:34:07 +0200

    Simplify/fix driver
    Signed-off-by: V10lator <v10lator@myway.de>

--- platform/platform_ARM_SUNxI_usb.c
+++ platform/platform_ARM_SUNxI_usb.c
@@ -27,26 +27,11 @@
 #include <drv_types.h>
 
 #ifdef CONFIG_PLATFORM_ARM_SUNxI
-extern int sunxi_usb_disable_hcd(__u32 usbc_no);
-extern int sunxi_usb_enable_hcd(__u32 usbc_no);
-extern int sunxi_wlan_get_usb_index(void);
-extern void sunxi_wlan_set_power(int on);
-#endif
-
-#if defined(CONFIG_PLATFORM_ARM_SUN6I) || defined(CONFIG_PLATFORM_ARM_SUN7I)
 extern int sw_usb_disable_hcd(__u32 usbc_no);
 extern int sw_usb_enable_hcd(__u32 usbc_no);
+static int usb_wifi_host = 2;
 extern void wifi_pm_power(int on);
-static script_item_u item;
 #endif
-
-#ifdef CONFIG_PLATFORM_ARM_SUN8I
-extern int sunxi_usb_disable_hcd(__u32 usbc_no);
-extern int sunxi_usb_enable_hcd(__u32 usbc_no);
-extern void wifi_pm_power(int on);
-static script_item_u item;
-#endif
-
 
 int platform_wifi_power_on(void)
 {
@@ -54,70 +39,7 @@ int platform_wifi_power_on(void)
 	int usb_no = sunxi_wlan_get_usb_index();
 
 	printk("platform_wifi_power_on(), usb_index: %d\n", usb_no);
-#ifdef CONFIG_PLATFORM_ARM_SUNxI
-#ifndef CONFIG_RTL8723A
-	{
-		/* ----------get usb_wifi_usbc_num------------- */
-		/*
-		ret = script_parser_fetch("usb_wifi_para", "usb_wifi_usbc_num", (int *)&usb_wifi_host, 64);
-		if(ret != 0){
-			DBG_8192C("ERR: script_parser_fetch usb_wifi_usbc_num failed\n");
-			ret = -ENOMEM;
-			goto exit;
-		}
-		DBG_8192C("sw_usb_enable_hcd: usbc_num = %d\n", usb_wifi_host);
-		sw_usb_enable_hcd(usb_wifi_host);
-		*/
-		sunxi_wlan_set_power(1);
-		mdelay(100);
-		sunxi_usb_enable_hcd(usb_no);
-	}
-#endif //CONFIG_RTL8723A
-#endif //CONFIG_PLATFORM_ARM_SUNxI
 
-#if defined(CONFIG_PLATFORM_ARM_SUN6I) || defined(CONFIG_PLATFORM_ARM_SUN7I)
-	{
-		script_item_value_type_e type;
-
-		type = script_get_item("wifi_para", "wifi_usbc_id", &item);
-		if(SCIRPT_ITEM_VALUE_TYPE_INT != type){
-			printk("ERR: script_get_item wifi_usbc_id failed\n");
-			ret = -ENOMEM;
-			goto exit;
-		}
-
-		printk("sw_usb_enable_hcd: usbc_num = %d\n", item.val);
-		wifi_pm_power(1);
-		mdelay(10);
-	
-		#if !(defined(CONFIG_RTL8723A)) && !(defined(CONFIG_RTL8723B))
-		sw_usb_enable_hcd(item.val);
-		#endif
-	}
-#endif //defined(CONFIG_PLATFORM_ARM_SUN6I) || defined(CONFIG_PLATFORM_ARM_SUN7I)
-
-#if defined(CONFIG_PLATFORM_ARM_SUN8I)
-	{
-		script_item_value_type_e type;
-
-		type = script_get_item("wifi_para", "wifi_usbc_id", &item);
-		if(SCIRPT_ITEM_VALUE_TYPE_INT != type){
-			printk("ERR: script_get_item wifi_usbc_id failed\n");
-			ret = -ENOMEM;
-			goto exit;
-		}
-
-		printk("sw_usb_enable_hcd: usbc_num = %d\n", item.val);
-		wifi_pm_power(1);
-		mdelay(10);
-	
-		#if !(defined(CONFIG_RTL8723A)) && !(defined(CONFIG_RTL8723B))
-		sunxi_usb_enable_hcd(item.val);
-		#endif
-	}
-#endif //CONFIG_PLATFORM_ARM_SUN8I
-
-exit:
 	return ret;
 }
 
@@ -129,26 +51,11 @@ void platform_wifi_power_off(void)
 #ifdef CONFIG_PLATFORM_ARM_SUNxI
 #ifndef CONFIG_RTL8723A
 	//DBG_8192C("sw_usb_disable_hcd: usbc_num = %d\n", usb_wifi_host);
-	//sw_usb_disable_hcd(usb_wifi_host);
-	sunxi_usb_disable_hcd(usb_no);
+	sw_usb_disable_hcd(item.val);
 	mdelay(100);
 	sunxi_wlan_set_power(0);
+//	wifi_pm_power(0);
 #endif //ifndef CONFIG_RTL8723A
 #endif	//CONFIG_PLATFORM_ARM_SUNxI
-
-#if defined(CONFIG_PLATFORM_ARM_SUN6I) || defined(CONFIG_PLATFORM_ARM_SUN7I)
-	#if !(defined(CONFIG_RTL8723A)) && !(defined(CONFIG_RTL8723B))
-	sw_usb_disable_hcd(item.val);
-	#endif
-	wifi_pm_power(0);
-#endif //defined(CONFIG_PLATFORM_ARM_SUN6I) || defined(CONFIG_PLATFORM_ARM_SUN7I)
-
-#if defined(CONFIG_PLATFORM_ARM_SUN8I)
-	#if !(defined(CONFIG_RTL8723A)) && !(defined(CONFIG_RTL8723B))
-	sunxi_usb_disable_hcd(item.val);
-	#endif
-	wifi_pm_power(0);
-#endif //defined(CONFIG_PLATFORM_ARM_SUN8I) 
-
 }
 
