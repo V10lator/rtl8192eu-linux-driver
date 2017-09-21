7b87e47d03a4dbea6dd5b4b2b581c6c51ae74b12
V10lator <v10lator@myway.de>
2017-09-21 08:50:32 +0200

    Simplify driver even more as USB is more like a x86 machine than what they call SUNxI on a C.H.I.P.
    Signed-off-by: V10lator <v10lator@myway.de>

--- platform/platform_ARM_SUNxI_usb.c
+++ platform/platform_ARM_SUNxI_usb.c
@@ -24,38 +24,12 @@
  *	
  */
  
-#include <drv_types.h>
-
-#ifdef CONFIG_PLATFORM_ARM_SUNxI
-extern int sw_usb_disable_hcd(__u32 usbc_no);
-extern int sw_usb_enable_hcd(__u32 usbc_no);
-static int usb_wifi_host = 2;
-extern void wifi_pm_power(int on);
-#endif
-
 int platform_wifi_power_on(void)
 {
-	int ret = 0;
-	int usb_no = sunxi_wlan_get_usb_index();
-
-	printk("platform_wifi_power_on(), usb_index: %d\n", usb_no);
-
-	return ret;
+	return 0;
 }
 
 void platform_wifi_power_off(void)
 {
-	int usb_no = sunxi_wlan_get_usb_index();
-
-	printk("platform_wifi_power_off(), usb_index: %d\n", usb_no);
-#ifdef CONFIG_PLATFORM_ARM_SUNxI
-#ifndef CONFIG_RTL8723A
-	//DBG_8192C("sw_usb_disable_hcd: usbc_num = %d\n", usb_wifi_host);
-	sw_usb_disable_hcd(item.val);
-	mdelay(100);
-	sunxi_wlan_set_power(0);
-//	wifi_pm_power(0);
-#endif //ifndef CONFIG_RTL8723A
-#endif	//CONFIG_PLATFORM_ARM_SUNxI
 }
 
